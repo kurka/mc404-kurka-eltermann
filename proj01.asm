edb8d0c689398201cbaf33debed00ccf72c69b20
felipe.eltermann <felipe.eltermann@b56fbafe-880a-11dd-82a6-aba1b78984bd>
2008-10-12 18:36:54 +0000

    Já está criando o arquivo de saída. Agora só falta implementar o processamento da imagem

--- proj01.asm
+++ proj01.asm
@@ -186,7 +186,7 @@ CarregaImagem:
 	mov ah, 0x3E
 	;; bx continua contendo o handle para o arquivo
 	int 0x21
-	jmp Next
+	;; continua para o inicio do processamento da imagem
 	
 	
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -195,8 +195,110 @@ CarregaImagem:
 	
 ; _____________________________________________________________ 
 	
-Next:	
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; processamento da imagem ;;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+	;; a altura e a largura da imagem estao, em pixels, gravadas em:
+	;; largura -> [es:img + 0x12] (word)
+	;; altura -> [es:img + 0x16] (word)
+	;; como a imagem tem no maximo 64k, certamente o numero de pixels
+	;; para qq uma das dimensoes eh <64k. Dessa forma, novamente,
+	;; apenas os dois primeiros bytes importam.
+	
+	;; o n de bytes que forma uma linha da img deve ser multiplo de 4
+	;; logo, deve-se somar um n de bytes a cada linha afim de normaliza-la
+	;; 0 <= (numero de bytes a mais / linha)-chamemos de 'apendice' <= 3
+
+	;; apendice = (largura*3)%4 [resto da divisao inteira]
+	mov ax, [es:img + 0x12]	; ax <- largura
+	mov bx, ax
+	shl ax, 1		; ax <- 2*ax
+	add ax, bx		; ax <- 2*ax + bx = 3*ax
+	and al, 0x03		; al <- ax%4
+	mov byte [apendice], al
+
+	
+	;; algoritmo do processamento (pseudo-codigo em alto nivel)
+	;; para i de 1 a altura {
+	;;   para j de 1 a largura {
+	;;     verifica(pixel)
+	;;     se (branco)-> continua no proximo pixel
+	;;     se (preto) -> verifica(vizinhos)
+	;;       enquanto houver vizinhos
+	;;         se(preto) -> passa para o proximo
+	;;         se(vermelho)-> passa para o proximo
+	;;         se(branco)-> pinta de vermelho
+	;;       fim enquanto
+	;;     fim se(preto)
+	;;   fim para(largura)
+	;; fim para(altura)
+
+	
+	;; Inicio
+	;; Como o arquivo nao apresenta a secao "'Color Map', a imagem
+	;; comeca em -> es:img + 0x36
+
+	;; Verificacao para cores:
+	;; BRANCO:   0xFF, 0xFF, 0xFF
+	;; PRETO:    0x00, 0x00, 0x00
+	;; VERMELHO: 0x00, 0x00, 0xFF
+	
+	
+	;; ********* !!! TEST AREA !!! ***********
+
+	;; pinta o primeiro byte de vermelho!!
+	mov word[es:img + 0x36],0x0000
+	 
+	;; ********* !!! TEST AREA !!! ***********
+
+
+	
+
+	;; continua para a geracao do arquivo de saida...
+	
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; processamento da imagem ;;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; _____________________________________________________________
+
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; geracao do arquivo de saida ;;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	;; criacao do arquivo
+	mov ah, 0x3C
+	mov cx, 0x00
+	mov dx, arq_out		; DS:DX -> apontador para o nome do arq em ASCIIZ
+	int 0x21
+
+	;; escrita no arquivo
+	mov bx, ax		; bx <- handle para arquivo de saida
+	mov cx, [tamanho_arq]
+	push ds			; guarda ds na pilha
+	mov ax, es
+	mov ds, ax		; ds <- (temporariamente) es
+	mov dx, img		; dx <- offset para a imagem
+	mov ah, 0x40		; servico de escrita
+	int 0x21
+
+	pop ds			; recupera ds
+
+	;; fecha arquivo de saida
+	mov ah, 0x3E
+	;; bx continua contendo o handle do arquivo de saida
+	int 0x21
+	
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; geracao do arquivo de saida ;;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+	
 Fim:
 	mov ah, 0x4C
 	int 0x21
@@ -210,7 +312,6 @@ arq_out: db 'saida.bmp',0x00
 
 ;; apontador (handle) para os arquivos de entrada e saida
 handle_arq_in: resb 2
-handle_arq_out: resb 2
 
 ;; msg de erro
 MsgErroFormato: db 'ERRO: Verifique se o nome do arquivo esta no formato imgXX.bmp',13,10,'$'
@@ -220,8 +321,11 @@ MsgErroArquivo:	db 'ERRO: O arquivo nao eh do tipo BMP especificado.',13,10,'$'
 ;; variaveis usadas na leitura do arquivo
 tipo_arq: resb 2
 tamanho_arq: resb 2
-	
 
+;; numero de bytes somado a cada linha (vai de 0 a 3)
+apendice: resb 1
+	
+	
 ;; este segmento (ES) eh para guardar o arquivo inteiro na memoria.
 SEGMENT data2
 img:	resb 0xFFF0	; ~64K (ao colocar 0xFFFF, o tlink diz que o segmento excede 64K)
