7a28c6fe9290782ed5bc5bd10e09beb24049d583
felipe.eltermann <felipe.eltermann@b56fbafe-880a-11dd-82a6-aba1b78984bd>
2008-11-30 02:15:52 +0000

    Acho que acabei a InsereRM-Destino-16

--- branches/proj02/InsereRM-Destino-16.asm
+++ branches/proj02/InsereRM-Destino-16.asm
@@ -78,11 +78,121 @@ InsereRM-Destino-16-mod11:
 ;;		DI <- indice para a proxima instrucao
 InsereMemoria:
 	mov byte[linha_de_comando + bx],'['
+	mov ah, al
+	and ah, 00000111b
+	
+	cmp ah, 00000000b
+	je InsereMemoria000
+	cmp ah, 00000001b
+	je InsereMemoria001
+	cmp ah, 00000010b
+	je InsereMemoria010
+	cmp ah, 00000011b
+	je InsereMemoria011
+	cmp ah, 00000100b
+	je InsereMemoria100
+	cmp ah, 00000101b
+	je InsereMemoria101
+	cmp ah, 00000110b
+	je InsereMemoria110
+	cmp ah, 00000111b
+	je InsereMemoria111
 
-
-
-
-
+InsereMemoria000:	
+	mov word[linha_de_comando + bx + 1],'BX'
+	mov byte[linha_de_comando + bx + 3],'+'
+	mov word[linha_de_comando + bx + 4],'SI'
+	add bx, 6
+	jmp FimInsereMemoria
+InsereMemoria001:	
+	mov word[linha_de_comando + bx + 1],'BX'
+	mov byte[linha_de_comando + bx + 3],'+'
+	mov word[linha_de_comando + bx + 4],'DI'
+	add bx, 6
+	jmp FimInsereMemoria
+InsereMemoria010:	
+	mov word[linha_de_comando + bx + 1],'BP'
+	mov byte[linha_de_comando + bx + 3],'+'
+	mov word[linha_de_comando + bx + 4],'SI'
+	add bx, 6
+	jmp FimInsereMemoria
+InsereMemoria011:
+	mov word[linha_de_comando + bx + 1],'BP'
+	mov byte[linha_de_comando + bx + 3],'+'
+	mov word[linha_de_comando + bx + 4],'DI'
+	add bx, 6
+	jmp FimInsereMemoria
+InsereMemoria100:
+	mov word[linha_de_comando + bx + 1],'SI'
+	add bx, 3
+	jmp FimInsereMemoria
+InsereMemoria101:	
+	mov word[linha_de_comando + bx + 1],'DI'
+	add bx, 3
+	jmp FimInsereMemoria
+InsereMemoria110:
+	mov word[linha_de_comando + bx + 1],'BP'
+	add bx, 3
+	jmp FimInsereMemoria
+InsereMemoria111:
+	mov word[linha_de_comando + bx + 1],'BX'
+	add bx, 3
+	jmp FimInsereMemoria
+	
+FimInsereMemoria:
+	call InsereDeslocamento
 	mov byte[linha_de_comando + bx], ']'
 	add bx, 1
 	ret
+
+
+;; Entradas: BX, DI, AL
+;; Saidas: BX, DI
+InsereDeslocamento:
+	mov ah, al
+	and ah, 11000000b
+
+	cmp ah, 00000000b
+	je InsereDeslocamento00
+	cmp ah, 01000000b
+	je InsereDeslocamento01
+	cmp ah, 10000000b
+	je InsereDeslocamento10
+	
+;; disp=0 (exceto para r/m=110)
+InsereDeslocamento00:
+	mov ah, al
+	and ah, 00000111b
+	cmp ah, 00000110
+	je InsereDeslocamento10
+	ret
+	
+;; disp=(disp-low)
+InsereDeslocamento01:
+	mov byte[linha_de_comando + bx], '+'
+	mov byte[linha_de_comando + bx + 1], '0'
+	push ax
+	inc di
+	call HexToAscii
+	mov word[linha_de_comando + bx + 2], ax
+	mov byte[linha_de_comando + bx + 4], 'h'
+	pop ax
+	add bx, 5
+	ret
+
+;; disp=(disp-high):(disp-low)
+InsereDeslocamento10:
+	mov byte[linha_de_comando + bx], '+'
+	mov byte[linha_de_comando + bx + 1], '0'
+	push ax
+	inc di
+	call HexToAscii
+	mov word[linha_de_comando + bx + 4], ax
+	inc di
+	call HexToAscii
+	mov word[linha_de_comando + bx + 2], ax
+	mov byte[linha_de_comando + bx + ], 'h'
+	pop ax
+	add bx, 7
+	ret
+
